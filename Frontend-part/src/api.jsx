566488de06b724a658a6dbbcb11547c02a876527
SamikshaMorya <moryasamiksha52@gmail.com>
2025-06-11 17:15:30 +0530

    added

--- Frontend-part/src/api.jsx
+++ Frontend-part/src/api.jsx
@@ -1,9 +1,10 @@
-// src/api.js (ya jahan bhi file ka naam ho)
+
+
 
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://backend-6-0m2f.onrender.com', 
+  baseURL: 'https://backend-6-0m2f.onrender.com/' 
 });
 
 export default API;

6732fc2a58883d6b1e686446153bc43f796241a8
SamikshaMorya <moryasamiksha52@gmail.com>
2025-06-14 10:40:54 +0530

    css modified

--- Frontend-part/src/components/Welcome.jsx
+++ Frontend-part/src/components/Welcome.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./Styles.css";
 
 const Welcome = () => {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <div style={{ fontSize: '18px', marginBottom: '20px' }}>
+    <div   style={{ textAlign: 'center', marginTop: '50px' }}>
+      <div className="front "  style={{ fontSize: '18px', marginBottom: '20px' }}>
         <Link to="/register" style={{ margin: '0 15px' }}>Register</Link>
         <Link to="/login" style={{ margin: '0 15px' }}>Login</Link>
         <Link to="/name" style={{ margin: '0 15px' }}>Submit Name</Link>
         <Link to="/edit-profile" style={{ margin: '0 15px' }}>Edit Profile</Link> 
       </div>
-      <h1>Welcome to the frontend!</h1>
+      <h1 >Welcome !</h1>
     </div>
   );
 };

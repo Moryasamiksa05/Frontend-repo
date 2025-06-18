3f5317f2851ee116601f8a13b9eec9456cc02b68
SamikshaMorya <moryasamiksha52@gmail.com>
2025-06-18 12:03:33 +0530

    added

--- Frontend-part/src/App.jsx
+++ Frontend-part/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import NameForm from './components/NameForm';

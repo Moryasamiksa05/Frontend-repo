3c6d3a2202e8ce4a1f0d8c9c7c77c95eefda291d
SamikshaMorya <moryasamiksha52@gmail.com>
2025-06-23 11:02:28 +0530

    app.jsx

--- Frontend-part/src/App.jsx
+++ Frontend-part/src/App.jsx
@@ -1,34 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
 import Register from './components/Register';
 import Login from './components/Login';
 import NameForm from './components/NameForm';
 import Dashboard from './components/Dashboard';
-
 import Welcome from './components/Welcome';
 import EditProfile from './components/EditProfile';
 import AdminLogin from './components/admin/AdminLogin';
 import AdminDashboard from './components/admin/AdminDashboard';
-
-
 import AdminRoutes from './routes/AdminRoutes';
 
 function App() {
   return (
     <Router>
       <Routes>
-      
+        {/* Public routes */}
         <Route path="/" element={<Welcome />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/name" element={<NameForm />} />
         <Route path="/edit-profile" element={<EditProfile />} />
 
-        {/* Admin routes with nesting */}
-        <Route path="/admin" element={<AdminRoutes />}>
+        
+        <Route path="/admin/*" element={<AdminRoutes />}>
           <Route path="login" element={<AdminLogin />} />
           <Route path="dashboard" element={<AdminDashboard />} />
         </Route>
--- Frontend-part/src/routes/AdminRoutes.jsx
+++ Frontend-part/src/routes/AdminRoutes.jsx
@@ -11,3 +11,4 @@ const AdminRoutes = () => {
 };
 
 export default AdminRoutes;
+
